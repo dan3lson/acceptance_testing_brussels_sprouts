38bd76b101cd94995c8308f64e4514ec15c7a639
Danelson Rosa <danelson.rosa@gmail.com>
2015-06-10 13:04:01 -0400

    Completed all integration tests.

--- spec/user_checks_a_recipe_spec.rb
+++ spec/user_checks_a_recipe_spec.rb
@@ -6,8 +6,8 @@ feature "User checks a recipe's deliciousness", %(
   So that I know with confidence what to cook.
 
   Acceptance Criteria:
-  [ ] When I visit the root path, I can see a form to submit a recipe name
-  [ ] If I submit a recipe name with "brussels sprouts" in the name, I am
+  [X] When I visit the root path, I can see a form to submit a recipe name
+  [X] If I submit a recipe name with "brussels sprouts" in the name, I am
       sent to a "results" page telling me that the recipe is delicious
   [ ] If I submit a recipe name without "brussels sprouts" in the name, I am
       sent to a "results" page telling me that the recipe is not delicious
@@ -19,22 +19,43 @@ feature "User checks a recipe's deliciousness", %(
 ) do
 
   scenario "user submits a recipe name containing 'brussels sprouts'" do
+    visit "/"
 
+    expect(page).to have_selector("form")
   end
 
   scenario "user submits a recipe name without 'brussels sprouts'" do
+    visit "/"
+    fill_in "Recipe Name", with: "brussels sprouts"
+    click_on "Submit"
 
+    expect(page).to have_content("\"brussels sprouts\" is a delicious recipe")
   end
 
   scenario "user navigates back to the home page after checking a recipe name" do
+    visit "/"
+    fill_in "Recipe Name", with: "brussels sprouts"
+    click_on "Submit"
+    click_on "Try again!"
 
+    expect(page).to have_content("Instructions")
   end
 
   scenario "user submits an empty form" do
+    visit "/"
+    fill_in "Recipe Name", with: ""
+    click_on "Submit"
 
+    expect(page).to have_content("Error!")
+    expect(page).to have_content("You can\'t submit an empty recipe name!")
   end
 
   scenario "user navigates back to the home page after submitting an empty form" do
+    visit "/"
+    fill_in "Recipe Name", with: ""
+    click_on "Submit"
+    click_on "Try again!"
 
+    expect(page).to have_content("Instructions")
   end
 end
